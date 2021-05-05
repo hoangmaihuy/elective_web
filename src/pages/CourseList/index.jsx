e787f304958ab7e1ea9a6555a15c3bd862681cc6
Mai Huy Hoang <maihuyhoang97@gmail.com>
2021-05-05 16:02:46 +0700

    Add SchoolEnum and CourseType

--- src/pages/CourseList/index.jsx
+++ src/pages/CourseList/index.jsx
@@ -2,9 +2,18 @@ import React, { useState, useRef } from 'react';
 import { PageContainer, FooterToolbar } from '@ant-design/pro-layout';
 import ProTable from '@ant-design/pro-table';
 import { getCourseList } from './service';
+import SchoolList from '@/consts/school';
+import CourseType from "@/consts/courseType";
 
 const CourseList = () => {
   const actionRef = useRef();
+  let schoolEnum = {}
+  // eslint-disable-next-line guard-for-in,camelcase
+  for (const school_id in SchoolList) {
+    schoolEnum[school_id] = {
+      text: SchoolList[school_id],
+    }
+  }
   const columns = [
     {
       title: '课程号',
@@ -15,18 +24,29 @@ const CourseList = () => {
       dataIndex: 'name',
     },
     {
+      title: '课程类型',
+      dataIndex: 'type',
+      valueEnum: CourseType,
+    },
+    {
       title: '学分',
       dataIndex: 'credit',
       valueType: 'number',
+      sorter: true,
+      hideInSearch: true,
     },
     {
       title: '开课单位',
       dataIndex: 'school_id',
+      valueEnum: SchoolList,
+      filter: true,
     },
     {
       title: '测评次数',
       dataIndex: 'review_count',
       valueType: 'digit',
+      sorter: true,
+      hideInSearch: true,
     },
   ];
   return (
