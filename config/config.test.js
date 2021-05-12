333b58fa96ce0889689efe3ea05a9fe6c04b432c
Mai Huy Hoang <maihuyhoang97@gmail.com>
2021-05-12 15:56:05 +0700

    Fix config

--- config/config.test.js
+++ config/config.test.js
@@ -14,7 +14,7 @@ export default defineConfig({
   locale: {
     // default zh-CN
     default: 'zh-CN',
-    antd: false,
+    antd: true,
     // default true, when it is true, will use `navigator.language` overwrite default
     baseNavigator: true,
   },
