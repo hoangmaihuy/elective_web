bd75663fd4cd0ad155a2847d08c3401d1e51900c
Mai Huy Hoang <maihuyhoang97@gmail.com>
2021-05-18 12:15:23 +0700

    Add review rate

--- src/pages/CourseDetail/index.jsx
+++ src/pages/CourseDetail/index.jsx
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import {PageHeader, Result} from "antd";
-import {connect, useParams} from "umi";
+import {Button, Result, Rate, Descriptions} from "antd";
+import {connect, history, useParams} from "umi";
 import SchoolList from '@/consts/school';
 import {Result as ApiResult} from '@/services/consts';
+import { GridContent } from '@ant-design/pro-layout';
+import { HomeOutlined, BookOutlined, ScheduleOutlined, NumberOutlined } from '@ant-design/icons';
+import { Avatar, Card, Col, Divider, Input, Row, Tag } from 'antd';
+import { roundToHalf } from "@/utils/utils";
+import styles from './style.less';
 
 
 const CourseDetail = (props) => {
   const { dispatch } = props;
-  const { status, courseInfo, courseReviews } = props;
-  const { fetchingCourseInfo, fetchingCourseReviews } = props;
+  const { status, courseInfo, teacherList, courseReviews } = props;
+  const { fetchingCourseInfo, fetchingTeacherList, fetchingCourseReviews } = props;
   const params = useParams();
   const courseId = parseInt(params.courseId, 10);
 
@@ -18,23 +23,123 @@ const CourseDetail = (props) => {
         type: "courseDetail/fetchCourseInfo",
         payload: courseId,
       })
+
+      dispatch({
+        type: "courseDetail/fetchTeacherList",
+        payload: courseId,
+      })
     }
   }, [courseId])
 
-  if (status === ApiResult.ERROR_COURSE_NOT_FOUND) {
+  if (!fetchingCourseInfo && status === ApiResult.ERROR_COURSE_NOT_FOUND) {
     return (
       <Result
         status={"404"}
-        title={"课程不存在"}
+        title={"404"}
+        subTitle={"课程不存在"}
+        extra={
+          <Button type="primary" onClick={() => history.push('/')}>
+            返回首页
+          </Button>
+        }
       />
     )
   }
 
+  const renderCourseInfo = () => {
+    const rateLabelStyle = {
+      marginTop: "8px",
+      width: "25%",
+    }
+
+    return (
+      <div>
+        <div className={styles.avatarHolder}>
+          <div className={styles.name}>{courseInfo.name}</div>
+        </div>
+        <div>
+          <Descriptions colon={false} labelStyle={rateLabelStyle} column={1}>
+            <Descriptions.Item label={"推荐分"}>
+              <Rate allowHalf disabled defaultValue={roundToHalf(courseInfo.recommendScore)}/>
+              <span style={{marginLeft: "8px"}}>
+                {courseInfo.recommendScore}
+              </span>
+            </Descriptions.Item>
+
+            <Descriptions.Item label={"课程内容"}>
+              <Rate allowHalf disabled defaultValue={roundToHalf(courseInfo.contentScore)}/>
+              <span style={{marginLeft: "8px"}}>
+              {courseInfo.contentScore}
+              </span>
+            </Descriptions.Item>
+
+            <Descriptions.Item label={"工作量"}>
+              <Rate allowHalf disabled defaultValue={roundToHalf(courseInfo.workScore)}/>
+              <span style={{marginLeft: "8px"}}>
+              {courseInfo.workScore}
+              </span>
+            </Descriptions.Item>
+
+            <Descriptions.Item label={"考核"}>
+              <Rate allowHalf disabled defaultValue={roundToHalf(courseInfo.examScore)}/>
+              <span style={{marginLeft: "8px"}}>
+              {courseInfo.examScore}
+              </span>
+            </Descriptions.Item>
+          </Descriptions>
+        </div>
+        <Divider dashed/>
+        <div className={styles.detail}>
+          <p>
+            <NumberOutlined className={styles.infoIcon}/>
+            {courseInfo.courseNo}
+          </p>
+          <p>
+            <HomeOutlined className={styles.infoIcon}/>
+            {courseInfo.schoolName}
+          </p>
+          <p>
+            <BookOutlined className={styles.infoIcon}/>
+            {courseInfo.typeName}
+          </p>
+          <p>
+            <ScheduleOutlined className={styles.infoIcon}/>
+            {`${courseInfo.credit} 学分`}
+          </p>
+        </div>
+      </div>
+    )
+  }
+
+  const renderTeacherList = () => {
+    return (
+      <div className={styles.tags}>
+        <div className={styles.tagsTitle}>任课老师</div>
+        {teacherList.map((teacher) => (
+          <Tag key={teacher.id}>{teacher.name}</Tag>
+        ))}
+      </div>
+    )
+  }
+
   return (
-    <PageHeader
-      title={courseInfo.name}
-      subTitle={`${courseInfo.schoolName} · ${courseInfo.credit}`}
-    />
+    <GridContent>
+      <Row gutter={24}>
+        <Col lg={7} md={24}>
+          <Card
+            bordered={false}
+            style={{
+              marginBottom: 24,
+            }}
+            loading={fetchingCourseInfo || fetchingTeacherList}
+          >
+            {renderCourseInfo()}
+            <Divider dashed/>
+            {renderTeacherList()}
+          </Card>
+        </Col>
+      </Row>
+    </GridContent>
   )
 }
 
@@ -42,6 +147,8 @@ export default connect(({ loading, courseDetail }) => ({
   status: courseDetail.status,
   courseInfo: courseDetail.courseInfo,
   fetchingCourseInfo: loading.effects["courseDetail/fetchCourseInfo"],
+  teacherList: courseDetail.teacherList,
+  fetchingTeacherList: loading.effects["courseDetail/fetchTeacherList"],
   courseReviews: courseDetail.courseReviews,
   fetchingCourseReviews: loading.effects[""]
 }))(CourseDetail);
--- src/pages/CourseDetail/model.js
+++ src/pages/CourseDetail/model.js
@@ -1,12 +1,15 @@
 import {getCourseInfo} from './service';
 import {Result} from '@/services/consts';
 import SchoolList from "@/consts/school";
+import CourseType from "@/consts/courseType";
+import {getTeachersByCourse} from "@/pages/AddReview/service";
 
 const Model = {
   namespace: 'courseDetail',
   state: {
     status: undefined,
     courseInfo: {},
+    teacherList: [],
     courseReviews: [],
   },
   effects: {
@@ -19,8 +22,18 @@ const Model = {
           courseInfo: reply,
         },
       });
-
     },
+
+    *fetchTeacherList({payload}, {call, put}) {
+      const {result, reply} = yield call(getTeachersByCourse, payload);
+      if (result === Result.SUCCESS) {
+        yield put({
+          type: 'saveTeacherList',
+          payload: reply.teachers,
+        })
+      }
+    },
+
   },
   reducers: {
     saveCourseInfo(state, {payload}) {
@@ -38,12 +51,23 @@ const Model = {
         courseInfo: {
           id: courseInfo.course_id,
           name: courseInfo.name,
+          courseNo: courseInfo.course_no,
+          type: courseInfo.type,
+          typeName: CourseType[courseInfo.type],
           schoolName: SchoolList[courseInfo.school_id],
           credit: courseInfo.credit,
           reviewCount: courseInfo.review_count,
+          recommendScore: courseInfo.recommend_score.toFixed(2),
+          contentScore: courseInfo.content_score.toFixed(2),
+          workScore: courseInfo.work_score.toFixed(2),
+          examScore: courseInfo.exam_score.toFixed(2),
         },
       }
     },
+
+    saveTeacherList(state, {payload}) {
+      return {...state, teacherList: payload}
+    }
 
   },
 };
