81add49222004c972f384876306d087488f46c11
Mai Huy Hoang <maihuyhoang97@gmail.com>
2021-05-31 21:12:27 +0700

    Add stop and remove container in script

--- scripts/pull_and_run_docker.sh
+++ scripts/pull_and_run_docker.sh
@@ -1,4 +1,8 @@
 echo "Pulling tuike_web image..."
 docker pull hoangmaihuy/tuike_web
+echo "Stop tuike_web container"
+docker container stop tuike_web
+echo "Remove tuike_web container"
+docker container rm tuike_web
 echo "Run tuike_web container on port 3000"
 docker run -p 3000:3000 -e "PORT=3000" --name tuike_web -d hoangmaihuy/tuike_web
